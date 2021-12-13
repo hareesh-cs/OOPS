124203b5e1f2327af6ed680445ceba3fbf7df4e8
hareesh-cs <hareeshsikakollu@gmail.com>
2021-12-13 17:51:21 +0530

    OOP's Initial Commit

---
+++ OOPs1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class OOPs1{
+	public static void main(String [] args){
+		Animal obj=new Animal("DOG");
+		String name=obj.getName();
+		System.out.println(name);
+	}
+}
+
+class Animal{
+	String name="";
+	public Animal(String name){
+		this.name=name;
+	}
+	public String getName(){
+		System.out.println("This is Animal class");
+		return this.name;
+	}
+}
